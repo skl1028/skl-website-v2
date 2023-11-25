1258b1e2dc34668de398798f13a43185a0e4c999
skl1028 <128899283+skl1028@users.noreply.github.com>
2023-11-25 08:12:11 +0000

    back db connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
-import os
 
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+db_connection_string = "mysql+pymysql://dfo8j20atq70zfbrdlz4:pscale_pw_xoFW4mmND7ByEGNM30NF6Y5Vsh3hhuCCzjjToFyRVjO@aws.connect.psdb.cloud/sklcareers?charset=utf8mb4"
 
 engine = create_engine(
   db_connection_string,

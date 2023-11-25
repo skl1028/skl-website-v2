de6a75b5d6602b9a8a2a587845f85790f4d84793
skl1028 <128899283+skl1028@users.noreply.github.com>
2023-11-25 07:38:40 +0000

    add secret

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import joblist_from_db
 
 app = Flask(__name__)
 
-def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs" ))
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row))
-    return jobs
-
 @app.route("/")
-def hello_world():
-  jobs = load_jobs_from_db()
+def hello_skl():
+  jobs = joblist_from_db()
   return render_template('home.html', 
-                           jobs=jobs, 
-                           company_name='SKL')
-
+                          jobs=jobs, 
+                          company_name='SKL')
 @app.route("/api/jobs")
 def list_jobs():
+  jobs = joblist_from_db()
   return jsonify(jobs)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+  return render_template('about.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,6 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://dfo8j20atq70zfbrdlz4:pscale_pw_xoFW4mmND7ByEGNM30NF6Y5Vsh3hhuCCzjjToFyRVjO@aws.connect.psdb.cloud/sklcareers?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(
   db_connection_string,
@@ -9,3 +10,12 @@ engine = create_engine(
       "ssl_ca": "/etc/ssl/cert.pem"
     }
   })
+
+def joblist_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._mapping))
+    return jobs
+  

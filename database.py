eb03f4568b141ff4d8babd9a1a9eedf6c23811ac
skl1028 <128899283+skl1028@users.noreply.github.com>
2023-11-25 09:53:26 +0000

    added connection to db

--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, text
+import os
 
-
-db_connection_string = "mysql+pymysql://dfo8j20atq70zfbrdlz4:pscale_pw_xoFW4mmND7ByEGNM30NF6Y5Vsh3hhuCCzjjToFyRVjO@aws.connect.psdb.cloud/sklcareers?charset=utf8mb4"
+db_connection = os.environ['DB_CONNECTION']
 
 engine = create_engine(
-  db_connection_string,
+  db_connection,
   connect_args={
     "ssl": {
       "ssl_ca": "/etc/ssl/cert.pem"
